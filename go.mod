c9fa71c56ec28a312dbd5f7b7f016ceaeb155a6c
aaronlelevier <aaron.lelevier@gmail.com>
2021-09-06 08:14:12 -0700

    Initial commit - go gin server

---
+++ go.mod
@@ -0,0 +1,5 @@
+module datatoapi.com/aaron/monday
+
+go 1.16
+
+require github.com/gin-gonic/gin v1.7.4 // indirect
---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.GET("/pyaaron", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pyaaron is going to show you his forehead",
+		})
+	})
+
+	r.POST("/loginJSON", func(c *gin.Context) {
+		var json Login
+		if err := c.ShouldBindJSON(&json); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		if json.User != "manu" || json.Password != "123" {
+			c.JSON(http.StatusUnauthorized, gin.H{"status": "unauthorized"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"status": "you are logged in"})
+	})
+
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
+
+type Login struct {
+	User     string `form:"user" json:"user" xml:"user"  binding:"required"`
+	Password string `form:"password" json:"password" xml:"password" binding:"required"`
+}
